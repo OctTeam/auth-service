fb19e08db7c7d2b5b3bd636e98323b0e894d35e1
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-20 21:54:29 +0300

    Add spring boot configuration

---
+++ src/main/java/com/oncteam/sg_store/auth_service/AuthService.java
@@ -0,0 +1,11 @@
+package com.oncteam.sg_store.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthService {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthService.class);
+    }
+}
