d510e8aafe5f98dfe62019e7cb00040e4900dc78
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-24 01:27:15 +0300

    Code refactor

--- src/main/java/com/sgs/auth/controller/AuthController.java
+++ src/main/java/com/sgs/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.sgs.auth.domain.response.TokenValidResponse;
 import com.sgs.auth.service.AuthService;
 import com.sgs.common.exceptions.*;
 import com.sgs.common.model.base.model.BaseResponse;
+import com.sgs.common.utils.SgsExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 
 import static com.sgs.auth.domain.utils.SecurityConstants.HEADER_TOKEN;
+import static com.sgs.common.utils.SgsCommonUtils.isEmpty;
+import static com.sgs.common.utils.SgsExceptionUtils.responseData;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -22,11 +25,9 @@ public class AuthController {
     @Autowired private AuthService service;
 
     @RequestMapping(value = "/auth/token", method = RequestMethod.POST)
-    public ResponseEntity createToken(@RequestBody TokenCreateRequest request, HttpServletResponse  response) throws Exception {
-        if (request==null || request.getUserId()==null) throw new EmptyRequestException();
-
+    public void createToken(@RequestBody TokenCreateRequest request, HttpServletResponse  response) throws Exception {
+        if (isEmpty(request)) throw new EmptyRequestException();
         response.addHeader(HEADER_TOKEN, service.createToken(request.getUserId()));
-        return new ResponseEntity(OK);
     }
 
     @RequestMapping(value = "auth/token", method = RequestMethod.GET)
@@ -34,22 +35,13 @@ public class AuthController {
         String token = headers.getFirst(HEADER_TOKEN);
         TokenValidResponse validResponse = new TokenValidResponse();
         validResponse.setValidToken(!service.isEmptyToken(token) && service.isActiveSession(token));
-        return new ResponseEntity(validResponse, HttpStatus.OK);
+        return ResponseEntity.ok(validResponse);
     }
 
     @ExceptionHandler
     public ResponseEntity<BaseResponse> dummyExceptionHandler(Exception e) {
-        if (e instanceof NotFoundException)
-            return new ResponseEntity<>(new BaseResponse(e.getMessage()), HttpStatus.NOT_FOUND);
-        if (e instanceof WrongPassOrEmailException)
-            return new ResponseEntity<>(new BaseResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        if (e instanceof EmptyRequestException)
-            return new ResponseEntity<>(new BaseResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        if (e instanceof EmailNotValidException)
-            return new ResponseEntity<>(new BaseResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        if (e instanceof PasswordDontMatchException)
-            return new ResponseEntity<>(new BaseResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(new BaseResponse("Unexpected exception"), HttpStatus.INTERNAL_SERVER_ERROR);
+        SgsExceptionUtils.ResponseData data = responseData(e);
+        return new ResponseEntity<>(data.getBody(), HttpStatus.valueOf(data.getCode()));
     }
 
 }
